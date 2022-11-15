5abaacb0fe517f534c0c0b33e17bcf70b8673a5a
Thigo <thiago.l17@yahoo.com>
2022-11-14 22:26:18 -0300

    Challenge 01: test units

---
+++ src/modules/statements/useCases/createStatement/CreateStatementUseCase.spec.ts
@@ -0,0 +1,68 @@
+import { hash } from "bcryptjs";
+import { InMemoryUsersRepository } from "../../../users/repositories/in-memory/InMemoryUsersRepository";
+import { OperationType } from "../../entities/Statement";
+import { CreateStatementError } from "./CreateStatementError";
+import { InMemoryStatementsRepository } from "../../repositories/in-memory/InMemoryStatementsRepository";
+import { CreateStatementUseCase } from "./CreateStatementUseCase"
+
+let createStatementUseCase: CreateStatementUseCase;
+let usersRepositoryInMemory: InMemoryUsersRepository;
+let statementsRepositoryInMemory: InMemoryStatementsRepository;
+
+describe('Create Statement', () => {
+  beforeEach(() => {
+    statementsRepositoryInMemory = new InMemoryStatementsRepository();
+    usersRepositoryInMemory = new InMemoryUsersRepository();
+    createStatementUseCase = new CreateStatementUseCase(
+      usersRepositoryInMemory,
+      statementsRepositoryInMemory
+    );
+  })
+
+  it('should be able to create a new statement', async () => {
+    const user = await usersRepositoryInMemory.create({
+      name: 'John Doe',
+      email: 'john@email.com',
+      password: await hash('1234', 8),
+    })
+
+    const statement = await createStatementUseCase.execute({
+      user_id: user.id,
+      type: OperationType.DEPOSIT,
+      amount: 100,
+      description: 'Deposit of 100'
+    });
+
+    expect(statement).toHaveProperty('id');
+  })
+
+  it('should not be able to create a new statement if user not found', async () => {
+
+    expect(async () => {
+      await createStatementUseCase.execute({
+        user_id: 'idInvalid',
+        type: OperationType.DEPOSIT,
+        amount: 100,
+        description: 'Deposit of 100'
+      });
+    }).rejects.toBeInstanceOf(CreateStatementError.UserNotFound);
+  })
+
+  it('should not be able to create a new statement if have insufficient funds', async () => {
+
+    expect(async () => {
+      const user = await usersRepositoryInMemory.create({
+        name: 'John Doe',
+        email: 'john@email.com',
+        password: await hash('1234', 8),
+      })
+
+      await createStatementUseCase.execute({
+        user_id: user.id,
+        type: OperationType.WITHDRAW,
+        amount: 100,
+        description: 'Deposit of 100'
+      });
+    }).rejects.toBeInstanceOf(CreateStatementError.InsufficientFunds);
+  })
+})
